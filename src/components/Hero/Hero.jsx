f7b4f77ba3bf3e8a7214f3d9e0fac295e713c7d6
joaoveasey <jovepedro@gmail.com>
2024-06-26 15:03:25 -0300

    first commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { getImageUrl } from "../../utils"
+import styles from "./Hero.module.css"
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+        <div className={styles.content}>
+            <h1 className={styles.title}>Oi, sou o João</h1>
+            <p className={styles.description}>
+              Sou estudante de tecnologia e estou em busca de uma oportunidade para ingressar na 
+              área de desenvolvimento de software.
+            </p>
+            <a href="mailto:jovepedro@gmail.com" className={styles.contactBtn}>
+              Contato
+            </a>
+        </div>
+        <img 
+          src={getImageUrl("hero/heroImage.png")} 
+          alt="Minha imagem" 
+          className={styles.heroImg}
+        />
+        <div className={styles.topBlur}></div>
+        <div className={styles.bottomBlur}></div>
+    </section>
+  )
+}
