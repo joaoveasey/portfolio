dc33c45fd0c1170940f4446e2ebd52c2f045c44f
joaoveasey <jovepedro@gmail.com>
2024-07-03 12:49:44 -0300

    melhoria "sobre"

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,7 +5,7 @@ import styles from "./About.module.css"
 export const About = () => {
   return (
     <section className={styles.container} id='about'>
-        <h2 className={styles.title}>Sobre</h2>
+        <h2 className={styles.title}>Sobre Mim</h2>
         <div className={styles.content}>
             <img 
                 src={getImageUrl("about/aboutImage.png")} 
@@ -14,27 +14,25 @@ export const About = () => {
             />
             <ul className={styles.aboutItems}>
                 <li className={styles.aboutItem}>
-                    <img src={getImageUrl("about/lastIcon.png")} alt="lastIcon" />
+                    <img src={getImageUrl("about/historia.png")} alt="Folha de Papel" />
                     <div className={styles.aboutItemText}>
-                        <h3>Estágio em TI</h3>
-                        <p>Faço estágio na empresa <a href="https://supley.com.br">Supley</a>, onde tenho a oportunidade de conhecer 
-                        mais sobre o ambiente de trabalho. (Out, 2023 - Presente)</p>
+                        {/* <h3>Sobre mim</h3> */}
+                        <p>Meu nome é João Veasey Pedro, tenho 19 anos e moro em Matão-SP. Comecei a estudar programação em 2022. Desde então, tenho investido continuamente em novos conhecimentos na área.</p>
+
                     </div>
                 </li>
-                <li className={styles.aboutItem}>
+                {/* <li className={styles.aboutItem}>
                     <img src={getImageUrl("about/cursorIcon.png")} alt="cursorIcon" />
                     <div className={styles.aboutItemText}>
-                        <h3>Desenvolvimento Front-end</h3>
-                        <p>No front-end tive contato com HTML, CSS e Javascript, 
-                        além do framework React para interfaces dinâmicas e responsivas.</p>
+                        <h3></h3>
+                        <p></p>
                     </div>
-                </li>
+                </li> */}
                 <li className={styles.aboutItem}>
-                    <img src={getImageUrl("about/serverIcon.png")} alt="cursorIcon" />
+                    <img src={getImageUrl("about/lastIcon.png")} alt="lastIcon" />
                     <div className={styles.aboutItemText}>
-                        <h3>Desenvolvimento Back-end</h3>
-                        <p>Estudo e aplico a linguagem C# no desenvolvimento back-end, 
-                            trabalhando com bancos de dados relacionais (SQL) e APIs REST.</p>
+                        {/* <h3>Estágio em TI</h3> */}
+                        <p>Atualmente, estou estagiando na empresa <a href="https://supley.com.br">Supley</a>. Esta experiência vem me permitindo conhecer como funciona o ambiente de trabalho e ver na prática os conhecimentos adquiridos na faculdade e cursos especializados.</p>
                     </div>
                 </li>
             </ul>
