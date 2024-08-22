06c73b720ae426c5dfa7823248c41a3a04f0f5fd
JOAO VEASEY PEDRO <2971392311010@fatecmatao.local>
2024-08-22 10:20:39 -0300

    implementing vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import { About } from "./components/About/About"
 import { Experience } from "./components/Experience/Experience"
 import { Projects } from "./components/Projects/Projects"
 import { Contact } from "./components/Contact/Contact"
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <div className={styles.App}>
+      <Analytics />
       <Navbar />
       <Hero />
       <About />
